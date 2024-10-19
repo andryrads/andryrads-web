0b15d73ac1bbb9f7e44bac2f9d4b4a8c79b81a36
andryrads <andry.rads@gmail.com>
2024-10-19 11:57:17 +0700

    finishing nav & main hero

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,37 @@
+"use client";
+
 import * as React from 'react';
 import styles from "./styles/navbar.module.scss";
+import { useState, useEffect } from 'react';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.navbar}>
-        <div>&nbsp;</div>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About Me</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#certification">Certification</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
+        <div>
+          <button className={styles.hamburger} onClick={toggleMenu}>
+            {isOpen ? <MenuOpenIcon /> : <MenuIcon />}
+          </button>
+        </div>
+        <div className={isOpen ? `${styles.menu} ${styles.open}` : styles.menu}>
+          <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About Me</a></li>
+              <li><a href="#skills">Skills</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#certification">Certification</a></li>
+              <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
